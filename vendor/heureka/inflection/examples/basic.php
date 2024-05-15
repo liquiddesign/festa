86da7f1d4cce8cc216b11b7345e8bdc6f6901cf5
Tomáš Pasičnyk <tomas@lqd.cz>
2024-05-15 08:23:27 +0200

    init

--- vendor/heureka/inflection/examples/basic.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../src/Inflection.php';
-
-$inflection = new Inflection();
-$inflected = $inflection->inflect('desítková soustava');
-
-print "Je potřeba znát " . $inflected[4] . PHP_EOL;
-// Je potřeba znát desítkovou soustavu
